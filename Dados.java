413b26b98444729af9612011085f1c17e3774b32
lp2141 <projetolp214.1@gmail.com>
2014-07-22 11:59:12 -0300

    Dados.java

---
+++ Dados.java
@@ -0,0 +1,57 @@
+public class Dados {
+    
+   private String email;
+   
+   private String senha;
+   
+   private String login;
+   
+   private String dicadesenha;
+    
+   
+   public Dados(){
+   
+   
+   }
+   
+   public String getEmail(){
+   
+   return email;
+   }
+   
+   public void setEmail(String msg){
+   
+   email = msg;
+   }
+   
+   public String getLogin(){
+   
+   return login;
+   }
+   
+   public void setLogin(String log ){
+   
+   login = log;
+   }
+   
+   public String getDicadesenha(){
+   
+   return dicadesenha;
+   }
+   
+   public void setDicadesenha(String ds){
+   
+   dicadesenha = ds;
+   }
+   
+   public String getSenha(){
+   
+   return senha;
+   }
+   
+   public void setSenha(String password){
+   
+   senha = password;
+   }
+   
+}
